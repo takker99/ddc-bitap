0e2b250d12dea002a24170463380e7cd3630506b
takker99 <37929109+takker99@users.noreply.github.com>
2022-02-11 14:52:03 +0900

    :bug: Calculated threshold using the wrong string length

--- denops/@ddc-filters/matcher_bitap.ts
+++ denops/@ddc-filters/matcher_bitap.ts
@@ -8,17 +8,16 @@ export class Filter extends BaseFilter<Params> {
       Params
     >,
   ): Promise<Candidate[]> {
-    const { test } = Asearch(` ${completeStr.trim()} `, { ignoreCase });
+    const source = ` ${completeStr.trim()} `;
+    const { test } = Asearch(source, { ignoreCase });
+    const threshold = Math.min(Math.floor(completeStr.trim().length / 2), 3) as
+      | 0
+      | 1
+      | 2
+      | 3;
 
     return Promise.resolve(
-      candidates.filter(({ word }) => {
-        const threshold = Math.min(Math.floor(word.length / 2), 3) as
-          | 0
-          | 1
-          | 2
-          | 3;
-        return test(word, threshold);
-      }),
+      candidates.filter(({ word }) => test(word, threshold)),
     );
   }
   override params(): Params {
